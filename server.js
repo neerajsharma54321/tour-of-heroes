5a0402c983e9a75795c6408f3db7239d9791bbac
naren-PC\naren <neerajsharma54321@gmail.com>
2018-02-07 12:07:56 -0800

    no message

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const app = express();
 
 app.use(express.static(_dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
+
+app.get('/*', function(){
+	res.sendFile(path.join(_dirname + 'dist/index.html'));
+});
+
+
+console.log('console listening');
