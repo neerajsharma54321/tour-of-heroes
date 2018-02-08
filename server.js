8e7cd280496ebc957834d11d4162eddcc97f52ac
neerajsharma54321 <neerajsharma54321@gmail.com>
2018-02-09 01:27:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express =  require("express");
 const app = express();
 
 app.use(express.static(_dirname + '/dist'));
-app.listen(process.env.PORT || 5000);
+app.listen(5000);
 
-app.get('/*', function(){
+app.get('/', function(){
 	res.sendFile(path.join(_dirname + 'dist/'));
 });
 
